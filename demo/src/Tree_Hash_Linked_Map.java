ff18c8a625d2b7ab465e74a98a5ca8359dfb28d9
Alexandr Alexeev <fart82@ukr.net>
2018-03-26 17:56:57 +0300

    commit from work pc

---
+++ demo/src/Tree_Hash_Linked_Map.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+
+public class Tree_Hash_Linked_Map {
+    public static void main(String args[]){
+        //HashMap - outputting element do`t safe sequence inputting
+        //TreeMap - outputting sort element
+        //LinkedHashMap - outputting elements as they are was inputted
+        TreeMap<String, Double> tm = new TreeMap<>();
+        tm.put("Ben Dou", 345.02);
+        tm.put("Ann Parker", 210.12);
+        tm.put("Carl Smith", 587.56);
+
+        Set<Map.Entry<String, Double>> set = tm.entrySet();
+        for(Map.Entry<String, Double> me : set){
+            System.out.print(me.getKey()+": ");
+            System.out.println(me.getValue());
+        }
+        Scanner scan = new Scanner(System.in);
+        String s = scan.nextLine();
+        double d = scan.nextDouble();
+
+        double dd = tm.get(s);
+        tm.put(s, dd + d);
+        for(Map.Entry<String, Double> me : set) {
+            System.out.print(me.getKey() + ": ");
+            System.out.println(me.getValue());
+        }
+    }
+}
--- demo/src/q.java
+++ demo/src/q.java
@@ -3,15 +3,13 @@ interface MyFunc<T> {
 }
 class HighTemp {
     private int hTemp;
-
     HighTemp(int ht) {
         hTemp = ht;
     }
-
     boolean sameTemp(HighTemp ht2) {
         return hTemp == ht2.hTemp;
     }
-        boolean lessThanTemp (HighTemp ht2){
+        boolean lessThanTemp (HighTemp ht2) {
             return hTemp < ht2.hTemp;
         }
     }
@@ -27,11 +25,14 @@ class InstanceMethWithObjectRefDemo{
         HighTemp [] weekDayHighs = { new HighTemp (89), new HighTemp (82), new HighTemp (90) , new HighTemp (89), new HighTemp (89), new HighTemp(91),new HighTemp (84), new HighTemp (83)};
         count = counter (weekDayHighs, HighTemp :: sameTemp, new HighTemp (89));
         System.out.println("Дней , когда максималь н а я температура бЬ1Ла 8 9 : " + count ) ;
+
         HighTemp [] weekDayHighs2 = {new HighTemp (32) ,new HighTemp (12), new HighTemp (24) ,new HighTemp (19), new HighTemp (18), new HighTemp (12), new HighTemp ( - 1 ) , new HighTemp (13)};
         count = counter ( weekDayHighs2 , HighTemp :: sameTemp, new HighTemp (12));
         System.out.println("Дней , когда максималь на я температур а была 12: " + count);
+
         count = counter ( weekDayHighs , HighTemp :: lessThanTemp, new HighTemp (89));
         System.out.println("Дней , когда максимал ь н а я температур а была ме ньше 8 9 : " + count);
+
         count = counter (weekDayHighs2, HighTemp :: lessThanTemp, new HighTemp (19));
         System.out.println("Дней , когда максималь н а я темпера тура была ме н ь ше 19 : " + count);
         }
