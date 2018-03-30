59987690e5ec4078b35fa132066d2f7362b171a1
Alexandr Alexeev <fart82@ukr.net>
2018-03-30 17:54:41 +0300

    commit from work pc

---
+++ demo/src/date.java
@@ -0,0 +1,14 @@
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+public class date {
+    public static void main(String args []){
+       Date d = new Date();
+
+        SimpleDateFormat format1 = new SimpleDateFormat("HH:mm:ss");
+        SimpleDateFormat format2 = new SimpleDateFormat("'Day' dd  \n'Month' MM  \n'Year' yyyy \n'Time zone' z");
+
+        System.out.println(format2.format(d));
+        System.out.println(format1.format(d));
+    }
+}
