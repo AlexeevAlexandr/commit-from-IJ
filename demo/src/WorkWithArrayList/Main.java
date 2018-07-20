839b72ef44b6bd8bec9613e2cf5c9f1289ceba6c
AlexeevAlexandr <https://github.com/AlexeevAlexandr>
2018-07-20 17:35:00 +0300

    commit from work pc

--- demo/src/WorkWithArrayList/Main.java
+++ demo/src/WorkWithArrayList/Main.java
@@ -12,46 +12,79 @@ public class Main {
 
                 String value = (String) view.write();
 
-                if (value.equals("help")){
-                    view.print("list availables commands:\n" +
-                            "add - add to list value\n" +
-                            "add by index - add to list on specific position\n" +
-                            "list - print list\n" +
-                            "exit - to exit");
-                }
-
-                if (value.equals("add")) {
-                    while(true) {
-                        view.print("add value or 'q' if you finished");
-                        Object values = view.write();
-                        if(values != ("q")) {
-                            arrayList.add(values);
-                        }else{break;}
-                    }
-                }
-
-                if (value.equals("add by index")) {
-                    view.print("add index from 1 to " + (arrayList.size()));
-                    int values1 = (view.writeInt() - 1);
-                    view.print("add value");
-                    Object values2 = view.write();
-                    arrayList.addByIndex((int)values1, values2);
-                }
-
-                if (value.equals("list")) {
-                    view.print("list:");
-                    if (arrayList.size() != 0) {
-                        view.print(arrayList.list.toString().replaceAll("[\\[\\]]", ""));
-                    }else{view.print("list is empty");}
-                }
-
-                if (value.equals("exit")) {
-                    view.print("goodbye, see you soon");
-                    check = false;
+                switch (value) {
+                    case "help":
+                        help(view);
+                        break;
+                    case "add":
+                        add(arrayList, view);
+                        break;
+                    case "add by index":
+                        addByIndex(arrayList, view);
+                        break;
+                    case "list":
+                        list(arrayList, view);
+                        break;
+                    case "exit":
+                        view.print("goodbye, see you soon");
+                        check = false;
+                        break;
+                    default:
+                        view.print("command not found");
+                        break;
                 }
             }catch (Exception e){
                 view.print("incorrect command");
             }
+        }
+    }
+
+    private static void addByIndex(ArrayListDemo arrayList, View view) {
+        while (true) {
+            try {
+                view.print("add value or 'q' if you finished");
+                Object value = view.write();
+                view.print("add index from 1 to " + (arrayList.size()));
+                int index = (view.writeInt() - 1);
+                if (!value.equals("q")) {
+                    arrayList.addByIndex(index, value);
+                    view.print("added");
+                } else {
+                    view.print("data to list is saved");
+                    break;
+                }
+            }catch (Exception e){view.print("incorrect index, try again");}
+        }
+    }
+
+    private static void add(ArrayListDemo arrayList, View view) {
+        while (true) {
+            view.print("add value or 'q' if you finished");
+            Object values = view.write();
+            if (!values.equals("q")) {
+                arrayList.add(values);
+                view.print("added");
+            } else {
+                view.print("data to list is saved");
+                break;
+            }
+        }
+    }
+
+    private static void help(View view) {
+        view.print("available commands:\n" +
+                "add - add to list value\n" +
+                "add by index - add to list on specific position\n" +
+                "list - print list\n" +
+                "exit - to exit");
+    }
+
+    private static void list(ArrayListDemo arrayList, View view) {
+        view.print("list:");
+        if (arrayList.size() != 0) {
+            view.print(arrayList.list.toString().replaceAll("[\\[\\]]", ""));
+        } else {
+            view.print("list is empty");
         }
     }
 }
