564e17d85b79285643dfb47ccd4a75ebaef14714
AlexeevAlexandr <https://github.com/AlexeevAlexandr>
2018-10-24 14:21:06 +0300

    collection

--- demo/src/Collection/List_ArrayList_LinkedList.java
+++ demo/src/Collection/List_ArrayList_LinkedList.java
@@ -6,17 +6,18 @@ import static java.lang.System.out;
 
 public class List_ArrayList_LinkedList {
     public static void main (String args[]){
-        //ArrayList -
-        //LinkedList -
+        //ArrayList
+        //LinkedList
         LinkedList<Integer> list = new LinkedList<>();
         list.add(2);//adds one element
         list.add(1);
         list.add(4);
-        list.add(3);
+        list.add(3,20);
         list.addFirst(5);//adds one element to first position
         list.addLast(3);//adds one element to last position
         list.set(2,8);//sets element to position of index "set(index, element)"
         Collections.addAll(list, 10,11,12);//adds multiple elements
+        list.remove(5);//remove object by index
         out.println("Original list: "+list);
 
         Collections.sort(list);//sort list
@@ -31,5 +32,7 @@ public class List_ArrayList_LinkedList {
         out.println("min: " + Collections.min(list));//find min element
 
         out.println("max: " + Collections.max(list));//find max element
+
+        out.println("Element by index: " + list.get(0));//get element by index
     }
 }
