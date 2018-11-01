ff8197c9c80ac4f32c251640209e1d0547c6927b
Alexandr Alexeev <fart82@ukr.net>
2018-11-01 23:40:23 +0200

    Map

--- demo/src/StreamAPI/Demo.java
+++ demo/src/StreamAPI/Demo.java
@@ -8,8 +8,8 @@ public class Demo {
     public static void main(String[] args) {
         List<String> list = Arrays.asList("a3","a1","a2","a2","a8");
 
-//        System.out.println(Arrays.stream(Arrays.toString(list.stream().mapToInt(a -> Integer.parseInt(a.substring(1))).toArray()).replaceAll("[\\[\\] ]","").split(",")).mapToInt(Integer::parseInt).sum());
-//        list.stream().sorted(Comparator.reverseOrder()).distinct().forEach(System.out::println);
+        System.out.println(Arrays.stream(Arrays.toString(list.stream().mapToInt(a -> Integer.parseInt(a.substring(1))).toArray()).replaceAll("[\\[\\] ]","").split(",")).mapToInt(Integer::parseInt).sum());
+        list.stream().sorted(Comparator.reverseOrder()).distinct().forEach(System.out::println);
         System.out.println(list.stream().max(String::compareTo).get());
         System.out.println(list.stream().min(String::compareTo).get());
     }
