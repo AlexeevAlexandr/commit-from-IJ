ddc6dbffd77b76c11096edad356854df2626e230
Alexandr Alexeev <fart82@ukr.net>
2018-11-18 22:28:32 +0200

    removing duplicate words

--- demo/src/IO/IO_Pattern_bridge/Pattern_bridge.java
+++ demo/src/IO/IO_Pattern_bridge/Pattern_bridge.java
@@ -1,16 +1,17 @@
 package IO.IO_Pattern_bridge;
 
 import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 
 /**
  * convert byte to char and conversely
  */
 public class Pattern_bridge {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         byte[] data = {72, 101, 108, 108, 111, 32, 87, 111, 114, 108, 100, 33};
         try(InputStream input = new ByteArrayInputStream(data);
-        Reader reader = new InputStreamReader(input, "UTF-8"))
+        Reader reader = new InputStreamReader(input, StandardCharsets.UTF_8))
         {
             char[] buffer = new char[11];
             reader.read(buffer);
@@ -18,7 +19,7 @@ public class Pattern_bridge {
         }catch (IOException e) { e.printStackTrace(); }
 
         try(ByteArrayOutputStream output = new ByteArrayOutputStream();
-            Writer writer = new OutputStreamWriter((OutputStream) output, "UTF-8"))
+            Writer writer = new OutputStreamWriter(output, StandardCharsets.UTF_8))
         {
             char[] chars = {'H', 'e', 'l', 'l', 'o', ' ', 'W', 'o', 'r', 'l', 'd', '!'};
             writer.write(chars);
