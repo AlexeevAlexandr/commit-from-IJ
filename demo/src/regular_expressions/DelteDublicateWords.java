8ebdefeccecd360d0cb107645801caf603a84104
Alexandr Alexeev <fart82@ukr.net>
2018-11-18 23:17:29 +0200

    currency payment format

--- demo/src/regular_expressions/DelteDublicateWords.java
+++ demo/src/regular_expressions/DelteDublicateWords.java
@@ -1,7 +1,9 @@
+package regular_expressions;
+
 import java.util.Arrays;
 import java.util.stream.Collectors;
 
-public class A {
+public class DelteDublicateWords {
 
     public static void main(String[] args) {
 
